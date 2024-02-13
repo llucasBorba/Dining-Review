8a8db1208f0b871030e057e76122be3fbd527d6e
llucasBorba <lucasborba6075@gmail.com>
2024-02-13 20:24:41 -0300

    User Controlle

--- src/main/java/com/project/Dining/Review/Controller/UserController.java
+++ src/main/java/com/project/Dining/Review/Controller/UserController.java
@@ -1,4 +1,43 @@
 package com.project.Dining.Review.Controller;
 
-public class UserController {
+import com.project.Dining.Review.Model.User;
+import com.project.Dining.Review.Repository.UserRepository;
+
+import java.util.Optional;
+
+public class UserController{
+
+    public UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User user){
+        var optionalUser = userRepository.findById(id);
+        if(optionalUser.isEmpty()){return null;}
+
+        var updatedUser = optionalUser.get();
+
+        if(user.getCity() != null){updatedUser.setName(user.getCity());}
+        if(user.getState() != null){updatedUser.setState(user.getState());}
+        if(user.getZipCode() != null){updatedUser.setZipCode(user.getZipCode());}
+        if(user.getInterestedInPeanutAllergies() != null){updatedUser.setInterestedInPeanutAllergies(user.getInterestedInPeanutAllergies());}
+        if(user.getInterestedInEggAllergies() != null){updatedUser.setInterestedInEggAllergies(user.getInterestedInEggAllergies());}
+        if(user.getInterestedInDairyAllergies() != null){updatedUser.setInterestedInDairyAllergies(user.getInterestedInDairyAllergies());}
+
+        return  userRepository.save(updatedUser);
+    }
+
+    public Optional<User> getByUserId(String id){
+        return userRepository.findById(id);
+    }
+    public boolean existsByDisplayName(String displayName) {
+        Optional<User> userOptional = userRepository.findById(displayName);
+        return userOptional.isPresent();
+    }
 }
